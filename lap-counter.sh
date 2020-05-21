0def5c828f59b16ef18536ac81b524f283d040b7
Jingbo Ni <jnistargazer@gmail.com>
2020-05-21 11:46:02 -0700

    Fixed path issues

--- lap-counter.sh
+++ lap-counter.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 BACKYARD_PATH=/usr/local/bin/backyard
 CONF=$BACKYARD_PATH/lap-counter/lap-counter.conf
+docker-compose -f $BACKYARD_PATH/lap-counter/docker-compose.yml down > /dev/null 2>&1 || true
 docker-compose -f $BACKYARD_PATH/lap-counter/docker-compose.yml up -d
 if [[ -f $CONF ]]; then
 	source $CONF
@@ -11,4 +12,4 @@ if [[ -f $CONF ]]; then
 		CAPTURE="--capture"
 	fi
 fi
-python3 $BACKYARD_PATH/LapCounterByCamCV.py $VIEW $CAPTURE --sensitivity ${SENSITIVITY:-"200"} --lap-length ${LAP_LENGTH:-"33"} > /tmp/laps-counter.log 2>&1
+python3 $BACKYARD_PATH/lap-counter/LapCounterByCamCV.py $VIEW $CAPTURE --sensitivity ${SENSITIVITY:-"200"} --lap-length ${LAP_LENGTH:-"33"} > /tmp/laps-counter.log 2>&1
