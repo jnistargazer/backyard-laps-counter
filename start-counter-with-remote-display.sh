165f7cb4a1a5ca24fb5e212b1ff79370dcaa0c2e
Jingbo Ni <jnistargazer@gmail.com>
2020-05-29 11:51:56 -0700

    Add a start script that can run lap-coiunter's video windows in a remote Xwindow server

---
+++ start-counter-with-remote-display.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+### We use SSH XForwarding.
+## Open the following lines in the remote host SSHD config in /etc/ssh/sshd_config:
+# X11Forwarding yes
+# X11DisplayOffset 10
+# X11UseLocalhost yes
+# PermitTTY yes
+# PrintMotd no
+# PrintLastLog yes
+# TCPKeepAlive yes
+
+# Must ssh -x pi@<my-ip-addr> if intending to run me from the remote host
+export DISPLAY=localhost:10.0
+python3 ./LapCounterByCamCV.py --view
