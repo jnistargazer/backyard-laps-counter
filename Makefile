90cde434d2051cdf91923f6a7f48d4fd5af80f00
Jingbo Ni <jnistargazer@gmail.com>
2020-05-30 15:11:46 -0700

    A hack: due to env issue, we have to add export LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 VERSION := "0.1"
 test:
 	docker-compose -f nginx/docker-compose.yml up -d
-	python3 LapCounterByCamCV.py
+	export LD_PRELOAD=${LD_PRELOAD}:/usr/lib/arm-linux-gnueabihf/libatomic.so.1 && python3 LapCounterByCamCV.py
 ui: nginx/index.html
 	cd nginx && make
 pkg:
--- lap-counter.sh
+++ lap-counter.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e -u -o pipefail
 BACKYARD_PATH=/usr/local/bin/backyard
 CONF=$BACKYARD_PATH/lap-counter/lap-counter.conf
 docker-compose -f $BACKYARD_PATH/lap-counter/docker-compose.yml down > /dev/null 2>&1 || true
@@ -13,4 +14,7 @@ if [[ -f $CONF ]]; then
 	fi
 fi
 #python3 $BACKYARD_PATH/lap-counter/LapCounterByCamCV.py $VIEW $CAPTURE --sensitivity ${SENSITIVITY:-"200"} --lap-length ${LAP_LENGTH:-"33"} > /tmp/laps-counter.log 2>&1
+
+# This is a hack. Should be removed if the lib can be found automatically
+export LD_PRELOAD=$LD_PRELOAD:/usr/lib/arm-linux-gnueabihf/libatomic.so.1
 python3 $BACKYARD_PATH/lap-counter/LapCounterByCamCV.py $VIEW $CAPTURE --sensitivity ${SENSITIVITY:-"200"} --lap-length ${LAP_LENGTH:-"33"}
