8dbba2c8101c486c2b93fb582123da4d66ad7092
Jingbo Ni <jnistargazer@gmail.com>
2020-05-30 16:19:31 -0700

    Make script work with -e -u options

--- lap-counter.sh
+++ lap-counter.sh
@@ -3,18 +3,19 @@ set -e -u -o pipefail
 BACKYARD_PATH=/usr/local/bin/backyard
 CONF=$BACKYARD_PATH/lap-counter/lap-counter.conf
 docker-compose -f $BACKYARD_PATH/lap-counter/docker-compose.yml down > /dev/null 2>&1 || true
-docker-compose -f $BACKYARD_PATH/lap-counter/docker-compose.yml up -d
-if [[ -f $CONF ]]; then
-	source $CONF
-	if [[ $VIEW == "true" || $VIEW == "yes" ]]; then
+docker-compose -f $BACKYARD_PATH/lap-counter/docker-compose.yml up -d || true
+VIEW=""
+CAPTURE=""
+source $CONF
+if [[ $VIEW == "true" || $VIEW == "yes" ]]; then
 		VIEW="--view"
-	fi
-	if [[ $CAPTURE == "true" || $CAPTURE == "yes" ]]; then
-		CAPTURE="--capture"
-	fi
+fi
+if [[ $CAPTURE == "true" || $CAPTURE == "yes" ]]; then
+	CAPTURE="--capture"
 fi
 #python3 $BACKYARD_PATH/lap-counter/LapCounterByCamCV.py $VIEW $CAPTURE --sensitivity ${SENSITIVITY:-"200"} --lap-length ${LAP_LENGTH:-"33"} > /tmp/laps-counter.log 2>&1
 
 # This is a hack. Should be removed if the lib can be found automatically
+LD_PRELOAD=${LD_PRELOAD:-""}
 export LD_PRELOAD=$LD_PRELOAD:/usr/lib/arm-linux-gnueabihf/libatomic.so.1
 python3 $BACKYARD_PATH/lap-counter/LapCounterByCamCV.py $VIEW $CAPTURE --sensitivity ${SENSITIVITY:-"200"} --lap-length ${LAP_LENGTH:-"33"}
