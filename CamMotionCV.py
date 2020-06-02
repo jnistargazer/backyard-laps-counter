fd5a26e60320a61a18bf8030bafdcf64342de88f
Jingbo Ni <jnistargazer@gmail.com>
2020-06-01 23:47:14 -0700

    Modify CamMotionCV.py for birding

--- CamMotionCV.py
+++ CamMotionCV.py
@@ -5,60 +5,22 @@ import time, os, sys
 import cv2
 
 class MotionDetector:
-    def __init__(self, src="picam", show=False, min_area=250):
-        self.src = src
-        self.show = show
+    def __init__(self, min_area=20):
         self.capture = False
         self.min_area = min_area
-        if self.src == "picam":
-            #self.vs = VideoStream(src=0).start()
-            self.vs = cv2.VideoCapture(0)
-            time.sleep(2.0)
-        else:
-            # otherwise, we are reading from a video file
-            self.vs = cv2.VideoCapture(self.src)
-
+        self.prevGrayedFrame = None
+        self.vs = cv2.VideoCapture(0)
+        time.sleep(2.0)
         if not self.vs.isOpened():
             print("Camera is not opened")
             sys.exit(1)
-    def set_capture(self, flag):
-        self.capture = flag
-    def detect_motion(self, motion_handler):
-        # loop over the frames of the video
-        motion_start = False
-        # initialize the first frame in the video stream
-        motionStartFrame = None
-        lap=0
-        is_a_lap = False
-        (w,h) = (int(self.vs.get(3)), int(self.vs.get(4)))
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        video_cap = None
-        fps = 10
-        cap_path = os.environ.get('CAPTURE_DIR', None)
-        cap_path = cap_path if cap_path else "./capture" 
-        os.makedirs(cap_path, exist_ok = True)
-        while True:
-            # grab the current frame and initialize the occupied/unoccupied
-            # text
-            ret,frame = self.vs.read()
-            #frame = frame if self.src == "picam" else frame[1]
-            # if the frame could not be grabbed, then we have reached the end
-            # of the video
-            if frame is None:
-                print("No frame available")
-                break
-            # resize the frame, convert it to grayscale, and blur it
-            frame1 = imutils.resize(frame, width=500)
-            gray = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-            gray = cv2.GaussianBlur(gray, (21, 21), 0)
-            # if the first frame is None, initialize it
-            if motionStartFrame is None:
-                motionStartFrame = gray
-                continue
+
+    def motion_detected(self, frame, gray):
+            frame = imutils.resize(frame, width=500)
             # compute the absolute difference between the current frame and
             # first frame
-            frameDelta = cv2.absdiff(motionStartFrame, gray)
-            thresh = cv2.threshold(frameDelta, 50, 255, cv2.THRESH_BINARY)[1]
+            delta = cv2.absdiff(self.prevGrayedFrame, gray)
+            thresh = cv2.threshold(delta, 20, 255, cv2.THRESH_BINARY)[1]
             # dilate the thresholded image to fill in holes, then find contours
             # on thresholded image
             thresh = cv2.dilate(thresh, None, iterations=2)
@@ -73,48 +35,70 @@ class MotionDetector:
                     continue
                 motion = True
                 break
-            if motion:
-                if not motion_start:
-                    #print("Motion started")
-                    motion_start = True
-                    is_a_lap = motion_handler.handle_motion(True)
-                    if self.capture and is_a_lap and video_cap is None:
-                        video_cap = cv2.VideoWriter(
-                                "{}/lap{}.avi".format(cap_path,lap),
-                                fourcc, fps, (w,h))
-                        lap += 1
+            return (motion, thresh, delta)
 
-                if video_cap is not None:
-                    video_cap.write(frame)
+    def detect_motion(self):
+        # loop over the frames of the video
+        motion_start = False
+        # initialize the first frame in the video stream
+        lap=0
+        is_a_lap = False
+        (w,h) = (int(self.vs.get(3)), int(self.vs.get(4)))
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        fps = 10
+        cap_path = "./capture" 
+        os.makedirs(cap_path, exist_ok = True)
+        timestamp = datetime.datetime.now().strftime("%m%d%Y-%I:%M:%S%p")
+        video_cap = cv2.VideoWriter(
+                  "{}/bird-{}.avi".format(cap_path,timestamp),
+                  fourcc, fps, (w,h))
+        while True:
+            # grab the current frame
+            T = time.time()
+            ret,frame = self.vs.read()
+            # resize the frame, convert it to grayscale, and blur it
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (21, 21), 0)
+            # if the first frame is None, initialize it
+            if self.prevGrayedFrame is None:
+                self.prevGrayedFrame = gray
+                continue
+            motion,thresh,delta = self.motion_detected(frame, gray)
+            self.prevGrayedFrame = gray
+            cv2.putText(frame,
+                      datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+                      (10, frame.shape[0] - 10),
+                      cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+            # show the frame and record if the user presses a key
+            cv2.imshow("Scene", frame)
+            #cv2.imshow("Thresh", thresh)
+            #cv2.imshow("Diff", delta)
+
+            if motion:
+                # Start recording timer if it isn't running
+                if T0 == 0:
+                    T0 = T
+
+            if T0 > 0 and T - T0 <= 10:  
+                # Keep recording 10s
+                video_cap.write(frame)
+            elif T0 > 0:
+                # If we come here, we know we have finished a 10s recording
+                # Turn off the timer
+                T0 = 0
             else:
-                is_a_lap = False
-                if motion_start:
-                    #print("Motion stopeed")
-                    motion_start = False
-                    motion_handler.handle_motion(False)
-                if video_cap:
-                    video_cap.write(frame)
-                    video_cap.release()
-                    video_cap = None
-            motionStartFrame = gray
-            if self.show:
-                text = "LAP {}".format(lap)
-                # draw the text and timestamp on the frame
-                cv2.putText(frame, text, (10, 20),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-                cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-                (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-                # show the frame and record if the user presses a key
-                cv2.imshow("Scene", frame)
-                cv2.imshow("Thresh", thresh)
-                cv2.imshow("Diff", frameDelta)
-                key = cv2.waitKey(1) & 0xFF
-                # if the `q` key is pressed, break from the lop
-                if key == ord("q"):
-                    break
+                # No motion detected as the timer is not started
+                pass
+            key = cv2.waitKey(1) & 0xFF
+            # if the `q` key is pressed, break from the lop
+            if key == ord("q"):
+                break
         # cleanup the camera and close any open windows
         if video_cap is not None:
             video_cap.release()
-        #self.vs.stop() if self.src == "picam" else self.vs.release()
         self.vs.release()
         cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    motion_sensor = MotionDetector(min_area=20)
+    motion_sensor.detect_motion()
--- lap-counter.sh
+++ lap-counter.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 set -e -u -o pipefail
-BACKYARD_PATH=/usr/local/bin/backyard
-CONF=$BACKYARD_PATH/lap-counter/lap-counter.conf
-docker-compose -f $BACKYARD_PATH/lap-counter/docker-compose.yml down > /dev/null 2>&1 || true
-docker-compose -f $BACKYARD_PATH/lap-counter/docker-compose.yml up -d || true
-VIEW=""
-CAPTURE=""
-source $CONF
-if [[ $VIEW == "true" || $VIEW == "yes" ]]; then
-		VIEW="--view"
-fi
-if [[ $CAPTURE == "true" || $CAPTURE == "yes" ]]; then
-	CAPTURE="--capture"
-fi
-#python3 $BACKYARD_PATH/lap-counter/LapCounterByCamCV.py $VIEW $CAPTURE --sensitivity ${SENSITIVITY:-"200"} --lap-length ${LAP_LENGTH:-"33"} > /tmp/laps-counter.log 2>&1
-
+CONF=./lap-counter.conf
 # This is a hack. Should be removed if the lib can be found automatically
 LD_PRELOAD=${LD_PRELOAD:-""}
 export LD_PRELOAD=$LD_PRELOAD:/usr/lib/arm-linux-gnueabihf/libatomic.so.1
-python3 $BACKYARD_PATH/lap-counter/LapCounterByCamCV.py $VIEW $CAPTURE --sensitivity ${SENSITIVITY:-"200"} --lap-length ${LAP_LENGTH:-"33"}
+python3 ./CamMotionCV.py
